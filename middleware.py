2670faa8e987b47aeef4966eaa2d72abb5201703
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-10-01 15:15:01 -0300

    UPDATE - middleware.py

--- middleware.py
+++ middleware.py
@@ -1,7 +1,17 @@
-from wepipe_python_sdk import Card
+import os
+
+from wepipe_python_sdk import Card, auth
+
+
+def login():
+    wepipe_domain = os.getenv("WEPIPE_DOMAIN")
+    wepipe_email = os.getenv("WEPIPE_EMAIL")
+    wepipe_password = os.getenv("WEPIPE_PASSWORD")
+    auth.login(wepipe_domain, wepipe_email, wepipe_password)
 
 
 def approval_compliance_to_approval_accountability(request):
+    login()
     card = Card(request=request)
     for contact in card.contacts:
         if contact.email == 'felipe.cabral@bluehealthcorporate.com':
@@ -11,6 +21,7 @@ def approval_compliance_to_approval_accountability(request):
 
 
 def approval_accountability_to_approval_financial(request):
+    login()
     card = Card(request=request)
     for contact in card.contacts:
         if contact.email == 'vinicius.dezotti@bluehealthcorporate.com':
