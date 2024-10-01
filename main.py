6cdb2f43cc23b0446705b69132fff12639120729
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-10-01 16:45:32 -0300

    UPDATE - main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI, APIRouter
 import middleware
 
+from wepipe_python_sdk import Card
+
 process = APIRouter(prefix="/process", tags=["process"])
 
 
 @process.post("/purchasing/approval_compliance_to_approval_accountability")
-async def approval_compliance_to_approval_accountability(request):
-    print(request)
+async def approval_compliance_to_approval_accountability(card: Card):
+    print(card)
     try:
-        middleware.approval_compliance_to_approval_accountability(request=request)
+        middleware.approval_compliance_to_approval_accountability(card=card)
         return {"Success": "True"}
     except Exception as e:
         return {"Error": str(e)}
 
 
 @process.post("/purchasing/approval_accountability_to_approval_financial")
-async def approval_accountability_to_approval_financial(request):
+async def approval_accountability_to_approval_financial(card: Card):
     try:
-        middleware.approval_accountability_to_approval_financial(request=request)
+        middleware.approval_accountability_to_approval_financial(card=card)
         return {"Success": "True"}
     except Exception as e:
         return {"Error": str(e)}
